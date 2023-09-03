412c7ed1835df97f445c72c724aa878745b13fca
Idir Aliane Crespo <idircarlos@gmail.com>
2023-09-03 15:43:08 +0200

    Removed macro for appending elements to vectors

--- src/cartridge.h
+++ src/cartridge.h
@@ -5,19 +5,6 @@
 #include "types.h"
 #include "mapper.h"
 
-#define DA_INIT_CAP 2048
-#define da_append(da, item)                                                          \
-    do {                                                                             \
-        if ((da)->count >= (da)->capacity) {                                         \
-            (da)->capacity = (da)->capacity == 0 ? DA_INIT_CAP : (da)->capacity*2;   \
-            (da)->items = realloc((da)->items, (da)->capacity*sizeof(*(da)->items)); \
-            assert((da)->items != NULL && "Not enough memory to allocate");          \
-        }                                                                            \
-        (da)->items[(da)->count++] = (item);                                         \
-    } while (0)
-
-
-
 typedef struct {
     u8 *items;
     usize size;
